efadcf8b5dd6c1db0ad4e5e5c7d4dd5816dd54c7
IoanaMargarint <ioana.margarint@s.unibuc.ro>
2026-04-02 00:55:18 +0300

    Update Evaluare.cpp

--- Evaluare.cpp
+++ Evaluare.cpp
@@ -1,6 +1,6 @@
 #include "Evaluare.h"
 
-Evaluare::Evaluare(int nota, std::string mesaj) : mesaj(mesaj) {
+Evaluare::Evaluare(int nota, const std::string& mesaj) : mesaj(mesaj) {
     if (nota < 1 || nota > 5) {
         throw ExceptieNotaInvalida();
     }
